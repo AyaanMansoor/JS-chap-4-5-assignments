a9c7a85ed72185e0a834217b5e8f091e5d378c01
AyaanMansoor <154600399+AyaanMansoor@users.noreply.github.com>
2024-03-17 17:47:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,256 @@
+
+//Q1
+
+// let num1 =+prompt("Enter First Number");
+// let num2 =+prompt("Enter Second Number");
+// let result = num1 + num2
+// document.write(result)
+
+//Q2
+
+// let num1 =+prompt("Enter First Number");
+// let num2=+prompt("Enter Second Number");
+// let result = num1 - num2
+// document.write(result)
+
+
+// let num1 =+prompt("Enter First Number");
+// let num2=+prompt("Enter Second Number");
+// let result = num1 * num2
+// document.write(result)
+
+
+// let num1=+prompt("Enter First Number");
+// let num2=+prompt("Enter Second Number");
+// let result = num1 / num2
+// document.write(result)
+
+
+//  let num1=+prompt("Enter First Number");
+//  let num2=+prompt("Enter Second Number");
+//  let result = num1 % num2
+//  document.write(result)
+
+
+
+
+//Q3
+// var a;
+// document .write("Value after variable declaration is : " + a +"<br>")
+
+// var a=5 ;
+// document.write("Initial Value", " ",a ,"<br>")
+
+// ++a;
+// document.write("Value after increment is:" , " ", +a+ "<br>")
+
+// a=a+7;
+// document.write("Value after addition is:",a, "<br>")
+
+// --a;
+// document.write("Value after decrement is: " + a + "<br>");
+
+// a = a % 3;
+// document.write("The remainder is: ", a);
+
+
+
+//Q4
+// var movieTicket=600
+// movieTicket=movieTicket*5
+// document.write ("Total cost to buy 5 tickets to a movie is ",movieTicket)
+
+
+//Q5
+// let userInput = +prompt("Enter a Table Number",4);
+
+// document.write(userInput + "x" + "1" + "=" + userInput*1 + "<br>");
+// document.write(userInput + "x" + "2" + "=" + userInput*2 +"<br>");
+// document.write(userInput + "x" + "3" + "=" + userInput*3+ "<br>");
+// document.write(userInput + "x" + "4" + "=" + userInput*4 +"<br>");
+// document.write(userInput + "x" + "5" + "=" + userInput*5 +"<br>");
+// document.write(userInput + "x" + "6" + "=" + userInput*6 +"<br>");
+// document.write(userInput + "x" + "7" + "=" + userInput*7 +"<br>");
+// document.write(userInput + "x" + "9" + "=" + userInput*9 +"<br>");
+// document.write(userInput + "x" + "10" + "=" +userInput*10 +"<br>");
+// document.write(userInput + "x" + "11" + "=" +userInput*11 + "<br>");
+// document.write(userInput + "x" + "12" + "=" + userInput*12 + "<br>");
+
+
+
+//Q7
+// let priceItem1=650;
+// let quantItem1=3;
+// let priceItem2=100;
+// let quantItem2=7;
+// let shippCharges=100;
+
+
+// let totalCostItem1 = priceItem1 * quantItem1;
+// let totalCostItem2 = priceItem2 * quantItem2;
+// let subTotal = totalCostItem1 + totalCostItem2;
+// let totalCost = subTotal+shippCharges;
+
+
+// document.write("Price of item 1 is" + " " + priceItem1 + "<br>")
+// document.write("Quantity of item 1 is" + " " + quantItem1 + "<br>")
+// document.write("Price of item 2 is" + " " + priceItem2 + "<br>")
+// document.write("Quantity of item 2 is" + " " + quantItem2 + "<br>")
+// document.write("Shipping Charges" + shippCharges + "<br>")
+// document.write("Total cost of your order is" + " " + totalCost)
+
+
+
+
+//Q6
+
+// let celsiusTemp = 25;
+// let fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+// document.write(celsiusTemp +"°C" + " " + "is"+ fahrenheitTemp + "<br>");
+
+
+// let fahrenheitTemp2 = 77;
+// let celsiusTemp2 = (fahrenheitTemp2 - 32) * 5/9;
+// document.write(fahrenheitTemp2 + "°F" + " " + "is" + celsiusTemp2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Q8
+// totalMarks=980
+// marksObt = 804
+// per = (marksObt/totalMarks) * 100
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + marksObt + "<br>");
+// document.write("Percentage: " + per + "% <br>");
+
+
+//Q9
+
+// let usdAmount = 10;
+// let sarAmount = 25;
+// let usdToPkrRate = 104.80;
+// let sarToPkrRate = 28;
+
+// let totalPkr = (usdAmount * usdToPkrRate) + (sarAmount * sarToPkrRate);
+// document.write("Total amount in Pakistani Rupees: " + totalPkr);
+
+
+//Q10
+// let num=5
+// let result=(((num+5)*10)/2)
+// document.write(result)
+
+
+//Q11
+// let currentYear =2024
+// let birthYear = 2007
+// let age= currentYear-birthYear
+// document.write(age)
+
+
+//Q12
+
+// let radius = 5; 
+// let circumference = 2 * 3.142 * radius;
+// let area = 3.142 * (radius * radius);
+// console.log("The circumference is " + circumference.toFixed(2));
+// console.log("The area is " + area.toFixed(2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Q13
+// let favoriteSnack = "chocolate";
+
+
+// let currentAge = 25;
+
+
+// let maxAge = 98;
+
+// let snacksPerDay = 2;
+// let yearsLeft = maxAge - currentAge
+// let totalSnacks = yearsLeft*365*snacksPerDay
+
+
+// document.write(" favorite snack: " + favoriteSnack + "<br>");
+// document.write(" current age: " + currentAge + "<br>");
+// document.write(" maximum age: " + maxAge + "<br>");
+// document.write(" amount of snacks per day: " + snacksPerDay + "<br>");
+// document.write("You will need " + totalSnacks + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + ".</p>");
+
+
+
+
+
+
+//CHAP04 PDF
+
+
+
+// Declare 3 variables in one statement
+//var variable1, variable2, variable3;
+
+// Declare 5 legal variable names
+// var myVariable;
+// var $anotherVariable;
+// var _variableName;
+// var camelCaseVariable;
+// var PascalCaseVariable;
+
+// Declare 5 illegal variable names
+// var 1stVariable; // Cannot start with a number
+// var my-variable; // Hyphens are not allowed
+// var @illegalVariable; // Special characters other than $ and _ are not allowed
+// var my variable; // Spaces are not allowed
+// var function; // Reserved keyword in JavaScript
+
+
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("b) Variable names can only contain , numbers, _, and  $.<br>For example $my_1stVariable<br>");
+// document.write("c) Variables must begin with a letter, $, or  _.<br>For example $name, _name, or name<br>");
+// document.write("d) Variable names are case sensitive.<br>");
+// document.write("e) Variable names should not be JS keywords.<br>");
+
+
+
+
+
+
+
+
+
